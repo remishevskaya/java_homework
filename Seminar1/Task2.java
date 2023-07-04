35944dff4af84503e80768f100975a477e325bb7
remishevskaya <heyjude945@gmail.com>
2023-07-04 23:15:58 +0300

    Сделала 2 задание

---
+++ Seminar1/Task2.java
@@ -0,0 +1,17 @@
+package Seminar1;
+
+public class Task2 {
+    public static void main(String[] args) {
+        boolean check = true;
+
+        for(int i = 2; i <= 1000; i++){
+            check = true;
+            for(int j = 2; j <= i; j++){
+                if(i % j == 0 && i != j) {
+                    check = false;
+                }
+            }
+            if(check == true) System.out.println(i);
+        }
+    }
+}
