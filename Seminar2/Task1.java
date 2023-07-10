b106ee3f6c06e5eb1661bcd6031ffb4a08599c03
remishevskaya <heyjude945@gmail.com>
2023-07-10 23:09:28 +0300

    Сделала задание 1

---
+++ Seminar2/Task1.java
@@ -0,0 +1,34 @@
+package Seminar2;
+
+import java.io.File;
+
+/**
+ * Task1
+ */
+public class Task1 {
+
+    public static void main(String[] args) {
+        String pathDirectory = System.getProperty("user.dir");
+        // String pathFile = pathDirectory.concat("\\Task1.java")
+        fileFormat(showCurrentFiles(pathDirectory));
+        
+    }
+
+    static String[] showCurrentFiles(String dirPath){
+        File currentDir = new File(dirPath);
+        return currentDir.list();
+    }
+
+    static void fileFormat(String [] dir){
+        for (String file : dir) {
+            String name = file.toString();
+            String[] format = name.split("[.]");
+            if(format.length > 1) {
+                System.out.printf("Расширение файла: %s\n", format[1]);
+            }
+            else {
+                System.out.println("Расширение файла: ");
+            }
+        }
+    }
+}
